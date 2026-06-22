05e5d052bc756f3681904381633662907691491f
Clinton141 <n0538894@gmail.com>
2026-06-22 23:51:06 +0100

    fix API base for production

--- DB.js
+++ DB.js
@@ -1,8 +1,9 @@
-const mysql = require("mysql2");
-require("dotenv").config();
-
 const db = mysql.createPool({
-    uri: process.env.DATABASE_URL,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT || 4000),
     ssl: {
         rejectUnauthorized: false
     }
@@ -16,5 +17,5 @@ db.getConnection((err, connection) => {
         connection.release();
     }
 });
-
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 module.exports = db;
