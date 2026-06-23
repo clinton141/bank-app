56f849cfd5369636c48f33ecc1ba78b69a0f10f0
Clinton141 <n0538894@gmail.com>
2026-06-23 06:51:50 +0100

    fix mysql connection using DATABASE_URL

--- DB.js
+++ DB.js
@@ -1,23 +1,10 @@
 const mysql = require("mysql2");
-require("dotenv").config();
 
-const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT || 4000),
-
-    ssl: {
-        rejectUnauthorized: false
-    },
-
-    connectTimeout: 10000
-});
+const db = mysql.createPool(process.env.DATABASE_URL);
 
 db.getConnection((err, connection) => {
     if (err) {
-        console.log("❌ DB Connection Error:", err);
+        console.log("❌ DB Connection Error:", err.message);
     } else {
         console.log("✅ TiDB Connected Successfully");
         connection.release();
