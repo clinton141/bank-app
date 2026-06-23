64f47aae1adff09dfa338647af2c5c6ebbbf7472
Clinton141 <n0538894@gmail.com>
2026-06-23 12:50:32 +0100

    fix DB.js export error

--- DB.js
+++ DB.js
@@ -1,15 +1,22 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const pool = mysql.createPool({
-    uri:process.env.DATABASE_URL,
+const db = mysql.createPool({
+    uri: process.env.DATABASE_URL,
 
     ssl: {
         rejectUnauthorized: false
-    },
-    connectionlimit:5,
-    waitforconnections: true,
-    queuelimit:0
+    }
+});
+
+// optional test connection (safe)
+db.getConnection((err, connection) => {
+    if (err) {
+        console.log("❌ DB Connection Error:", err.message);
+    } else {
+        console.log("✅ TiDB Connected Successfully");
+        connection.release();
+    }
 });
 
 module.exports = db;
