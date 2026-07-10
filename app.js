4ff5d8c43523ddcca765b264b24ecd86012ac0d2
Clinton141 <n0538894@gmail.com>
2026-07-10 19:54:53 +0100

    update system

--- app.js
+++ app.js
@@ -1557,7 +1557,7 @@ app.get("/user/has-active-investment/:phone", (req, res) => {
 app.get("/ezeaguuy/all-users", (req, res) => {
 
     const sql = `
-        SELECT id, phone,investments,total_returns, status, balance
+        SELECT id, phone,status, balance
         FROM users
         ORDER BY id DESC
     `;
